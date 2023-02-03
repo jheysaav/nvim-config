bfaaf5b4b93ce7a2803a6916971dc308b3d8bbd3
Jheyson Saavedra <hello@jheysaav.me>
2023-02-03 11:57:44 -0500

    feat(fish): add node version manager

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ ln -sf $PWD/fish $HOME/.config
 ln -sf $PWD/nvim $HOME/.config
 ln -sf $PWD/starship.toml $HOME/.config/starship.toml
 
+echo "Setting nvm..."
+
+set --universal nvm_data $HOME/.local/share/nvm || export nvm_data $HOME/.local/share/nvm
